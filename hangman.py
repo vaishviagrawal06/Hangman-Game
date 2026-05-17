3aa9fb30e662c7f1ae79f3baf7f2878c7a788204
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-05-17 09:32:02 +0530

    game

---
+++ hangman.py
@@ -0,0 +1,15 @@
+import random
+
+print("Welcom to Hangman Game!")
+
+# difficulty level choose karna hai
+
+print("\n choose difficulty level!")
+print("1. Easy")
+print("2. Medium")
+print("3. Hard")
+
+choice = input("ENTER CHOICE (1/2/3): ")
+
+if choice=="1":
+    
